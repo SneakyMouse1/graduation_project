3e5e83dc39ddd04bbb4f3218f8c3e090674e2e65
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-07-11 21:59:43 +0200

    Modal windows

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import scroll from "./modules/scroll";
 import calculate from "./modules/calculate";
+import modal from "./modules/modal";
 
-timer("15 july 2022");
+timer("21 july 2022 12:00");
 scroll();
 calculate(1000);
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,39 @@
+export const modal = () => {
+	const modal = document.querySelector(".header-modal");
+	const overlay = document.querySelector(".overlay");
+	const btn = document.getElementsByClassName("btn")[1];
+	const closeBtn = document.querySelector(".header-modal__close");
+
+	btn.addEventListener("click", (event) => {
+		event.preventDefault();
+		overlay.style.display = "block";
+		modal.style.display = "block";
+	});
+
+	closeBtn.addEventListener("click", () => {
+		overlay.style.display = "none";
+		modal.style.display = "none";
+	});
+	const serviceBtn = document.querySelectorAll(".service-button");
+	const servicesModal = document.querySelector(".services-modal");
+	const servicesClose = document.querySelector(".services-modal__close");
+	const overlaytwo = document.querySelector(".overlay");
+	const subj = servicesModal.querySelector('input[name="subject"]');
+
+	serviceBtn.forEach((btn) => {
+		btn.addEventListener("click", (event) => {
+			event.preventDefault();
+			overlaytwo.style.display = "block";
+			servicesModal.style.display = "block";
+			subj.value = event.target.attributes[1].value;
+		});
+	});
+
+	servicesClose.addEventListener("click", (event) => {
+		event.preventDefault();
+		overlaytwo.style.display = "none";
+		servicesModal.style.display = "none";
+	});
+};
+
+export default modal;
