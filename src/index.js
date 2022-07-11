4127f07bc4c8d8157bd5df8f538f2e666bfebb35
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-07-11 22:28:37 +0200

    Lightbox (modal_IMG)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import scroll from "./modules/scroll";
 import calculate from "./modules/calculate";
 import modal from "./modules/modal";
+import lightbox from "./modules/lightbox";
 
 timer("21 july 2022 12:00");
 scroll();
 calculate(1000);
 modal();
+lightbox();
---
+++ src/modules/lightbox.js
@@ -0,0 +1,42 @@
+export const lightbox= () => {  
+    const body = document.querySelector('body');
+    const certificateDocuments = document.querySelectorAll('.sertificate-document');
+    const modalOverlay = document.querySelector('.overlay');
+
+    const modalBlock = document.createElement('div');
+    const clone = modalOverlay.cloneNode(true);
+
+    certificateDocuments.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+
+            // Getting the original IMG SRC
+            let href = e.target.closest('.sertificate-document').getAttribute('href'); 
+            
+            const imgDocument = document.createElement('img');
+            imgDocument.setAttribute('src', href);
+            
+            body.append(modalBlock);
+            modalBlock.append(imgDocument);
+            modalBlock.append(clone);
+
+            imgDocument.style.display = 'block';
+            imgDocument.style.position = 'fixed';
+            imgDocument.style.top = '50%';
+            imgDocument.style.left = '50%';
+            imgDocument.style.zIndex = '9999';
+            imgDocument.style.transform = 'translate(-50%, -50%)';    
+            imgDocument.style.maxHeight = '95%';
+            clone.style.display = 'block';
+
+            modalBlock.addEventListener('click', () => {
+                modalBlock.innerHTML = '';
+                modalBlock.remove();
+            });
+        });
+    });
+
+    
+};
+
+export default lightbox;
