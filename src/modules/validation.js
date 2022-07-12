9940687456498450936db72b3ace5739a60f1648
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-07-12 13:08:37 +0200

    Update validation.js

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,17 +1,17 @@
 export const validation = () => {
     const formName = document.querySelectorAll('[placeholder="ваше имя"]')
-    const formTelephone = document.querySelectorAll('[placeholder="ваш телефон"]')
+    const formTelephone = document.querySelectorAll('[placeholder="ваш телефон"]');
 
     formName.forEach(formName =>
         formName.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^а-яА-Я\^a-zA-z@\-\s]/g, "") 
-    }))
+            e.target.value = e.target.value.replace(/[^а-яА-Я\^a-zA-z@\-\s]/g, "") ;
+    }));
 
     formTelephone.forEach(formTelephone =>
         formTelephone.addEventListener('input', (e) => {
-            e.target.value = e.target.value.replace(/[^0-9\(\)\+]/g, "")
+            e.target.value = e.target.value.replace(/[^0-9\(\)\+]/g, "");
         })
-    )
-}
+    );
+};
     
 export default validation;
