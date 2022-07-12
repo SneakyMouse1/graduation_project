d86f030be76089effda8d93260b5ff2d135579c4
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-07-12 11:03:55 +0200

    service slider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import scroll from "./modules/scroll";
 import calculate from "./modules/calculate";
 import modal from "./modules/modal";
 import lightbox from "./modules/lightbox";
+import serviceSlider from "./modules/serviceSlider";
 
 timer("21 july 2022 12:00");
 scroll();
 calculate(1000);
 modal();
 lightbox();
+serviceSlider();
