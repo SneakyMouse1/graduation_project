763c26d345e368f5d8444cb40a96f50bf1b89fa3
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-07-12 12:15:24 +0200

    slider fix (service + benefit)

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import scroll from "./modules/scroll";
 import calculate from "./modules/calculate";
 import modal from "./modules/modal";
 import lightbox from "./modules/lightbox";
-import serviceSlider from "./modules/serviceSlider";
+import slider from "./modules/slider";
+// import sendForm from "./modules/sendForm";
 
 timer("21 july 2022 12:00");
 scroll();
 calculate(1000);
 modal();
 lightbox();
-serviceSlider();
+slider();
